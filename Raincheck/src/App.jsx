ed5df0bf9320ca25d144e4b7a3d14a5c4012e1ff
Marcus Heneborn <heneborn@gmail.com>
2024-04-22 12:28:23 +0200

    some fixes

--- Raincheck/src/App.jsx
+++ Raincheck/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import SearchBar from "./components/SearchBar";
+import "./App.css";
 
 function App() {
   const [data, setData] = useState(null);
@@ -40,17 +41,21 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Search for Weather</h1>
       <SearchBar onSearch={handleSearch} />
-      {data && data.current ? (
-        <div>
-          <h2>Current Weather in {location}</h2>
-          <p>Temperature: {data.current.temp_c} °C</p>
-          <p>Condition: {data.current.condition.text}</p>
-        </div>
-      ) : (
-        <p>Search for a location to find the weather.</p>
-      )}
+      <div
+        className={`info-container ${data && data.current ? "slide-down" : ""}`}
+      >
+        {data && data.current ? (
+          <>
+            <h2>{location}</h2>
+            <img src={data.current.condition.icon} alt="Weather Icon" />
+            <p>Temperature: {data.current.temp_c} °C</p>
+            <p>Condition: {data.current.condition.text}</p>
+          </>
+        ) : (
+          <p>Search for a location to find the weather.</p>
+        )}
+      </div>
     </div>
   );
 }
---
+++ Raincheck/src/components/FormComponents.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const StyledInput = styled.input`
+  font-size: 16px;
+  border: 1px solid black;
+  height: 32px;
+  background-color: white;
+  margin-right: 16px;
+  color: black;
+`;
+
+export const StyledButton = styled.button`
+  text-align: center;
+  font-size: 16px;
+  background-color: white;
+  color: black;
+`;
